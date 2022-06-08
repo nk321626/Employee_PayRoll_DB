1445fcf62af23b12f74b265e54aed9548541c1aa
nk321626 <nk321626@gmail.com>
2022-06-08 17:05:20 +0530

    Added Employee payroll Data UC3

--- Employee_PayRoll_DB/SQLQuery2.sql
+++ Employee_PayRoll_DB/SQLQuery2.sql
@@ -10,3 +10,11 @@ name varchar(35),
 salary money,
 start_date date
 )
+
+-------------UC3---------------
+insert into employee_payroll values
+('Ganesh',12000.0,'2018-06-09'),
+('Tomesh',30000.0,'2022-01-07'),
+('Niraj',33300.0,'2020-09-09'),
+('Abhishek',45000.0,'2022-08-07'),
+('Urvi',39000.0,'2021-09-01')
