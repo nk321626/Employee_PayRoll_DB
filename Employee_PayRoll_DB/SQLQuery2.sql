7ba6c408a95cb49d81d0f03caabb0d8d53c4c1cb
nk321626 <nk321626@gmail.com>
2022-06-08 16:58:51 +0530

    Added Employee Payroll Table UC2

--- Employee_PayRoll_DB/SQLQuery2.sql
+++ Employee_PayRoll_DB/SQLQuery2.sql
@@ -1,3 +1,12 @@
 ﻿------------UC1----------------
 create database payroll_server2
 use payroll_server2
+
+------------UC2----------------
+create table employee_payroll
+(
+id int identity,
+name varchar(35),
+salary money,
+start_date date
+)
