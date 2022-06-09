59be86104050b4d15d99ce8a9f42554075da7f5c
nk321626 <nk321626@gmail.com>
2022-06-09 22:53:47 +0530

    Added Gender to all person UC6

---
+++ Employee_PayRoll_DB/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿use Payroll_Service
+select * from Payroll_Service
+
+----------UC6----------------
+alter table payroll_service add gender varchar(1)
+
+update payroll_service set gender='M' where name in('Ganesh','Tomesh','Niraj','Abhishek')
+
+update Payroll_Service set gender='F' where name in ('Urvi');
